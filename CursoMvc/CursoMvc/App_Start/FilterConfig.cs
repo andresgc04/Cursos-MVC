a4c2a42b066e82187e5c43f8710ecf7eef935432
andresgc04 <andresgc1997@outlook.com>
2021-11-21 01:00:41 -0400

    Agregar Filtros y Seguridad

--- CursoMvc/CursoMvc/App_Start/FilterConfig.cs
+++ CursoMvc/CursoMvc/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace CursoMvc
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filters.VerifySession());
         }
     }
 }
