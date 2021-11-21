95499a52575326c620ca743b3c3294f3a69e581e
andresgc04 <andresgc1997@outlook.com>
2021-11-21 12:23:49 -0400

    Agregar Funcionalidad para registrar nuevos usuarios y validar los campos necesarios

--- CursoMvc/CursoMvc/Controllers/UserController.cs
+++ CursoMvc/CursoMvc/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CursoMvc.Models;
 using CursoMvc.Models.TablesViewsModels;
+using CursoMvc.Models.ViewsModels;
 
 namespace CursoMvc.Controllers
 {
@@ -18,18 +19,46 @@ namespace CursoMvc.Controllers
             using (CursosDbEntities db = new CursosDbEntities())
             {
                 lst = (from d in db.Usuarios
-                      where d.Id_Estado == 1
-                      orderby d.Email
-                      select new UserTableViewModel
-                      {
-                          Id_Usuario = d.Id_Usuario,
-                          Email = d.Email,
-                          Edad = d.Edad
-                      }).ToList();
+                       where d.Id_Estado == 1
+                       orderby d.Email
+                       select new UserTableViewModel
+                       {
+                           Id_Usuario = d.Id_Usuario,
+                           Email = d.Email,
+                           Edad = d.Edad
+                       }).ToList();
             }
 
 
             return View(lst);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(UserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            using (var db = new CursosDbEntities())
+            {
+                Usuarios usuariosObj = new Usuarios();
+                usuariosObj.Id_Estado = 1;
+                usuariosObj.Email = model.Email;
+                usuariosObj.Edad = model.Edad;
+                usuariosObj.Password = model.Password;
+
+                db.Usuarios.Add(usuariosObj);
+
+                db.SaveChanges();
+            }
+
+            return Redirect(Url.Content("~/User/"));
         }
     }
 }
---
+++ CursoMvc/CursoMvc/Models/ViewsModels/UserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoMvc.Models.ViewsModels
+{
+    public class UserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "El {0} debe tener al menos {1} caracteres", MinimumLength = 1)]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirmar Contraseña")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "La contraseñas no son iguales")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
+}
