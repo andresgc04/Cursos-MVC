f6895916f2230987f137daf7531ec31c4d346c8f
andresgc04 <andresgc1997@outlook.com>
2021-11-21 20:34:09 -0400

    Agregar Funcionalidad para subir archivos al servidor

---
+++ CursoMvc/CursoMvc/Controllers/FilesController.cs
@@ -0,0 +1,41 @@
+﻿using CursoMvc.Models.ViewsModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoMvc.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            if (TempData["Message"] != null)
+                ViewBag.Message = TempData["Message"].ToString();
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(FileViewModel model)
+        {
+            string PathSite = Server.MapPath("~/");
+            string PathFile1 = Path.Combine(PathSite + "/Files/file1.png");
+            string PathFile2 = Path.Combine(PathSite + "/Files/file2.png");
+
+            if (!ModelState.IsValid)
+                return View("Index", model);
+
+            model.File1.SaveAs(PathFile1);
+            model.File2.SaveAs(PathFile2);
+
+            @TempData["Message"] = "Se cargaron los archivos";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CursoMvc/CursoMvc/Models/ViewsModels/FileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CursoMvc.Models.ViewsModels
+{
+    public class FileViewModel
+    {
+        [Required]
+        [DisplayName("Mi Archivo:")]
+        public HttpPostedFileBase File1 { get; set; }
+
+        [Required]
+        [DisplayName("Mi Archivo 2:")]
+        public HttpPostedFileBase File2 { get; set; }
+
+        [Required]
+        [DisplayName("Mi Cadena:")]
+        public string Cadena { get; set; }
+    }
+}
